4ccc76f0cb0fb08f17ad0dffc3e42ac65e5a341d
godbod <gkounkou@gmail.com>
2018-05-07 21:45:16 -0700

    Pull request for Gradle setup

---
+++ src/main/java/core/TemplateTest.java
@@ -0,0 +1,14 @@
+/**
+ * TemplateTest class this is a comment.
+ */
+public class TemplateTest {
+
+   /**
+     This is just a constructor
+     @param args the main function parameter
+   */
+   public static void main(String [] args) {
+      Template o = new Template("Jacques");
+      System.out.println(o.sayHello());
+   }
+}
